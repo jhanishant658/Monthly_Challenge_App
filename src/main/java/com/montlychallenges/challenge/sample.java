1326759fe4aee0839eb0e50f0b5b66ed6a1ff36f
Nishant jha <jhanishant658@gmail.com>
2025-08-01 10:41:53 +0530

    Added sample

---
+++ src/main/java/com/montlychallenges/challenge/sample.java
@@ -0,0 +1,11 @@
+package com.montlychallenges.challenge;
+
+public class sample {
+    // This is a sample class with a main method
+    // It can be used for testing or demonstration purposes
+    // You can add methods and fields as needed
+
+    public static void main(String[] args) {
+        System.out.println("Running the sample class main method.");
+    }
+}
