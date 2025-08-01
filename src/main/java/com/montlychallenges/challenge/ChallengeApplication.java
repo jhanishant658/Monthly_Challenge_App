8470bbe5cff81156cc9e76c2c0e3f3bedaa42f68
Nishant jha <jhanishant658@gmail.com>
2025-08-01 10:39:22 +0530

    Update ChallengeApplication.java
    project initiated

--- src/main/java/com/montlychallenges/challenge/ChallengeApplication.java
+++ src/main/java/com/montlychallenges/challenge/ChallengeApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ChallengeApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Starting Monthly Challenges sjcbdjbApplication...");
 
 		SpringApplication.run(ChallengeApplication.class, args);
 	}
