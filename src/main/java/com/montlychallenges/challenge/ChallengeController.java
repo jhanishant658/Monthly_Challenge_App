662dbebba2fb8f7278db0c33f5d776ca07f24edf
Nishant jha <jhanishant658@gmail.com>
2025-08-07 14:33:51 +0530

    Update ChallengeController and add trace DB file
    Modified ChallengeController.java and updated the main H2 database file. Added challengeDB.trace.db for H2 database tracing, likely to assist with debugging or tracking database issues.

--- src/main/java/com/montlychallenges/challenge/ChallengeController.java
+++ src/main/java/com/montlychallenges/challenge/ChallengeController.java
@@ -1,25 +1,16 @@
 package com.montlychallenges.challenge;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PutMapping;
 
-
-
-
-
-
+@CrossOrigin(origins = "http://localhost:3000", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 @RestController
+
 public class ChallengeController {
    
 
